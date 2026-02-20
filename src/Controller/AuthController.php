1f9c6f977164eee4ef2abe1c606a9a25ecf633d9
Jan Linhart <admin@escope.cz>
2026-02-20 17:55:00 +0100

    Adding auth creds to the login call back

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,12 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 
 final class AuthController extends AbstractController
 {
+    public function __construct(
+        #[Autowire(env: 'AUTH0_DOMAIN')]
+        private readonly string $auth0Domain,
+        #[Autowire(env: 'AUTH0_CLIENT_ID')]
+        private readonly string $auth0ClientId,
+    ) {
+    }
+
     public function callback(): Response
     {
-        return $this->render('auth/callback.html.twig');
+        return $this->render('auth/callback.html.twig', [
+            'auth0_domain' => $this->auth0Domain,
+            'auth0_client_id' => $this->auth0ClientId,
+        ]);
     }
 }
